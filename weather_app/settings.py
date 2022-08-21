71fb179a09932c66b8e626fa05a9bf21a1edae08
Cell <suhail9453531504@gmail.com>
2022-08-21 15:19:02 +0530

    edit settings.py

--- weather_app/settings.py
+++ weather_app/settings.py
@@ -26,9 +26,9 @@ SECRET_KEY = config("SECRET_KEY")
 WEATHER_API_KEY = config('WEATHER_API_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
